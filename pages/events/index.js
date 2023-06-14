48cfcb4d2973763293de69079af87bf4f5f65cbd
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-14 17:05:06 +0530

    2 files modified

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,6 +3,110 @@ import Footer from '../../components/Footer';
 import Widgets from '../../components/Widgets';
 import Link from 'next/link';
 // import { Alert } from 'flowbite-react';
+const EVENTS = [
+  {
+    id: 1,
+    event_dates: [
+      { event_date: '2014-02-18T18:00:00' },
+      { event_date: '2014-02-25T18:00:00' },
+      { event_date: '2014-03-04T18:00:00' },
+      { event_date: '2014-03-11T18:00:00' },
+    ],
+    neighborhood_name: 'The Loop',
+    description: 'Open City hosts a weekly event in which...',
+    venue_name: '1871',
+    url: 'https://opengovhacknight.eventbrite.com/',
+    price: 'Free',
+    title: 'Open Gov Hack Night',
+    state: 'IL',
+    city_name: 'Hubli',
+  },
+  {
+    id: 2,
+    event_dates: [
+      { event_date: '2014-02-18T18:00:00' },
+      { event_date: '2014-02-25T18:00:00' },
+      { event_date: '2014-03-04T18:00:00' },
+      { event_date: '2014-03-11T18:00:00' },
+    ],
+    neighborhood_name: 'The Loop',
+    description: 'Open City hosts a weekly event in which...',
+    venue_name: '1871',
+    url: 'https://opengovhacknight.eventbrite.com/',
+    price: 'Free',
+    title: 'Open Gov Hack Night',
+    state: 'IL',
+    city_name: 'Hubli',
+  },
+  {
+    id: 3,
+    event_dates: [
+      { event_date: '2014-02-18T18:00:00' },
+      { event_date: '2014-02-25T18:00:00' },
+      { event_date: '2014-03-04T18:00:00' },
+      { event_date: '2014-03-11T18:00:00' },
+    ],
+    neighborhood_name: 'The Loop',
+    description: 'Open City hosts a weekly event in which...',
+    venue_name: '1871',
+    url: 'https://opengovhacknight.eventbrite.com/',
+    price: 'Free',
+    title: 'Open Gov Hack Night',
+    state: 'IL',
+    city_name: 'Hubli',
+  },
+  {
+    id: 5,
+    event_dates: [
+      { event_date: '2014-02-18T18:00:00' },
+      { event_date: '2014-02-25T18:00:00' },
+      { event_date: '2014-03-04T18:00:00' },
+      { event_date: '2014-03-11T18:00:00' },
+    ],
+    neighborhood_name: 'The Loop',
+    description: 'Open City hosts a weekly event in which...',
+    venue_name: '1871',
+    url: 'https://opengovhacknight.eventbrite.com/',
+    price: 'Free',
+    title: 'Open Gov Hack Night',
+    state: 'IL',
+    city_name: 'Hubli',
+  },
+  {
+    id: 6,
+    event_dates: [
+      { event_date: '2014-02-18T18:00:00' },
+      { event_date: '2014-02-25T18:00:00' },
+      { event_date: '2014-03-04T18:00:00' },
+      { event_date: '2014-03-11T18:00:00' },
+    ],
+    neighborhood_name: 'The Loop',
+    description: 'Open City hosts a weekly event in which...',
+    venue_name: '1871',
+    url: 'https://opengovhacknight.eventbrite.com/',
+    price: 'Free',
+    title: 'Open Gov Hack Night',
+    state: 'IL',
+    city_name: 'Hubli',
+  },
+  {
+    id: 7,
+    event_dates: [
+      { event_date: '2014-02-18T18:00:00' },
+      { event_date: '2014-02-25T18:00:00' },
+      { event_date: '2014-03-04T18:00:00' },
+      { event_date: '2014-03-11T18:00:00' },
+    ],
+    neighborhood_name: 'The Loop',
+    description: 'Open City hosts a weekly event in which...',
+    venue_name: '1871',
+    url: 'https://opengovhacknight.eventbrite.com/',
+    price: 'Free',
+    title: 'Open Gov Hack Night',
+    state: 'IL',
+    city_name: 'Hubli',
+  },
+];
 import {
   MainComponent,
   Button,
@@ -110,22 +214,43 @@ export default function Events() {
             Discover upcoming virtual events
           </h1>
           <div className="grid grid-cols-3 md:grid-cols-3 ">
-            <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
-              <h3>
-                <Link href="./event/efficient">
-                  Efficient iOS App Development with Bazel
-                </Link>
-              </h3>
-              <div></div>
-            </div>
-            <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
-              <h3>The Self-Taught Programmers Path Way</h3>
-              <div></div>
-            </div>
-            <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
-              <h3>Distributed Systems Architectural Patterns</h3>
-              <div></div>
-            </div>
+            {EVENTS &&
+              EVENTS.map((item) => {
+                return (
+                  <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
+                    <h3>
+                      <Link href={`./event/${item.id}`}>
+                        {item.description + item.id}
+                      </Link>
+                    </h3>
+                    <div className="flex text-green-600 mt-2 mb-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                        />
+                      </svg>
+                      <span className="font-bold font-size-md ">
+                        Watch Recording
+                      </span>
+                    </div>
+                    <div></div>
+                  </div>
+                );
+              })}
           </div>
           <div className="font-bold text-color-pink-100">
             See more events ->
@@ -134,18 +259,43 @@ export default function Events() {
             Watch recordings of popular past events
           </h1>
           <div className="grid grid-cols-3 md:grid-cols-3 ">
-            <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
-              <h3>Efficient iOS App Development with Bazel</h3>
-              <div></div>
-            </div>
-            <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
-              <h3>The Self-Taught Programmers Path Way</h3>
-              <div></div>
-            </div>
-            <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
-              <h3>Distributed Systems Architectural Patterns</h3>
-              <div></div>
-            </div>
+            {EVENTS &&
+              EVENTS.map((item) => {
+                return (
+                  <div className="m-2 p-2 border-2 border-color-pink-100 hover:bg-gray-100 hover:border-pink-500 hover:cursor-pointer">
+                    <h3>
+                      <Link href={`./event/${item.id}`}>
+                        {item.description + item.id}
+                      </Link>
+                    </h3>
+                    <div className="flex text-green-600 mt-2 mb-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                        />
+                      </svg>
+                      <span className="font-bold font-size-md ">
+                        Watch Recording
+                      </span>
+                    </div>
+                    <div></div>
+                  </div>
+                );
+              })}
           </div>
           <div className="font-bold text-color-pink-100">
             See more events ->
