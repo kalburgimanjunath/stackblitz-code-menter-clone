729482665db07e49cf4979a86ff605adc00fe0c4
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-14 16:14:10 +0530

    1 file modified, 3 added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Widgets from '../components/Widgets';
-// import styles from '../styles/Home.module.css';
-import { Alert } from 'flowbite-react';
-import { Tabs } from 'flowbite-react';
+// import { Alert } from 'flowbite-react';
 import {
   MainComponent,
   Button,
